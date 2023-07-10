753f985b6fc80de2f5295de5ca8ba2987b115de4
guilhermedelucas <gdelucasde@gmail.com>
2023-07-10 18:41:49 +0200

    Initial commit

---
+++ files.py
@@ -0,0 +1,26 @@
+import os
+
+FILEPATH = 'todos.txt'
+
+if not os.path.exists(FILEPATH):
+    with open(FILEPATH, 'w') as file:
+        pass
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items"""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(content, filepath=FILEPATH):
+    """ Write the to-do items list in the text file """
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(content)
+
+
+if __name__ == '__main__':
+    print('Hello')
+    print(get_todos())
---
+++ web.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from files import get_todos, write_todos
+
+st.title('My Todo App')
+st.subheader('This is my todo app.')
+st.write('This app is to increase your productivity.')
+
+
+todos = get_todos()
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+
+def add_todo():
+    todo_local = st.session_state['new_todo']
+    todos.append(todo_local + '\n')
+    write_todos(todos)
+    st.session_state['new_todo'] = ''
+
+
+st.text_input(label="Add new todo...",
+              placeholder="Add new todo...",
+              on_change=add_todo,
+              key="new_todo",
+              label_visibility='collapsed')
